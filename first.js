0f562d726e0c13146608f4027d0f5fc37452dab6
Akhilesh <akhilraikumar@gmail.com>
2025-01-11 12:20:34 +0530

    as

---
+++ first.js
@@ -0,0 +1,17 @@
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     greet: function() {
+//         console.log('Hello, ' + this.firstName);
+//     }
+// };
+
+// // Accessing properties and methods
+// console.log(person.firstName); // John
+// person.greet(); // Hello, John
+// console.log(person.age);
+if(true){
+  var x=5;
+}
+   console.log(x);
